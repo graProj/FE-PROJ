2f9cb1ab792a07267941751c87b2da30d9e385ea
unknown <201910825>
2024-08-23 15:41:32 +0900

    원격 수정

--- project/src/pages/lectureRoom.jsx
+++ project/src/pages/lectureRoom.jsx
@@ -1,9 +1,6 @@
-import React  from "react";
+import React, { useEffect }  from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { io } from "socket.io-client";
 import { sendImage } from "../api/sendImage";
-import { Alert } from "../components/ui/alert";
-
 function LectureRoom() {
     const navigate = useNavigate();
     const {id} = useParams()
@@ -17,19 +14,12 @@ function LectureRoom() {
       sendImage(roomId, source);
       console.log(source);
     }, 10000);
-
+    useEffect(() => {
+      navigate(`/home/room/L${roomId}M${userId}`);
+    }, []);
     return (
-        <div className="flex">
-          <div className="flex-1 p-5 border border-black">
-            <h2>교수 화면</h2>
-            <video id="peerVideo" playsInline autoPlay className="w-[300px] h-[300px]" />
-          </div>
-          <div className="flex-3 p-5">
-            <h2>학생 화면</h2>
-            <video id="myFace" playsInline autoPlay className="w-[40%] h-[30%]" />
-          </div>
-          <button onClick={() => { navigate(`/home/room/L${roomId}M${userId}`); }}>원격실 입장</button>
-            
+        <div className="flex bg-black" >
+          
         </div>
       );
     }
